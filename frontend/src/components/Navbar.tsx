bb061c31afb0eb5059724a0d2bb693707341e012
cyw37 <cyw37@drexel.edu>
2025-05-06 16:58:05 -0400

    Fixed tailwind config

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar: React.FC = () => {
+  return (
+    <nav className="flex flex-row w-full space-between items-center bg-gray-800 top-0">
+        <div className="flex flex-row mx-auto px-4 py-4 justify-between items-center">
+            <div>Home</div>
+            <div>Home</div>
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Hero.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+
+const Hero = (props: Props) => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
