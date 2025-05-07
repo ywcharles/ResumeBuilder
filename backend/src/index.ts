e481ad6843adb3e24b95b01c1a6d24fafab111bd
navingeorge <navin.george10@gmail.com>
2025-05-06 23:54:52 -0400

    setup routing structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import sqlite3 from "sqlite3";
 import * as sqlite from "sqlite";
 import * as url from "url";
 import path, { parse } from "path";
+import initAuthRoutes from './routes/auth.js';
 
 let __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 let dbfile = path.resolve(__dirname, "..", "database.db");
@@ -15,6 +16,9 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.json());
+
+// Initialize routes
+app.use("/auth", initAuthRoutes(db));
 
 app.get('/api/hello', (_req, res) => {
   res.json({ message: 'Hello from the backend!' });
